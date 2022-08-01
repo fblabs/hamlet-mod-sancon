864ca9510578b30130576622d1f010aea4b21778
Fabio Bonfante <info@fblabs.eu>
2022-08-01 14:00:31 +0200

    2.9.9.7.002

--- release/moc_hmodifyprod.cpp
+++ release/moc_hmodifyprod.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_HModifyProd_t {
-    QByteArrayData data[28];
-    char stringdata0[439];
+    QByteArrayData data[32];
+    char stringdata0[539];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -33,38 +33,42 @@ struct qt_meta_stringdata_HModifyProd_t {
 static const qt_meta_stringdata_HModifyProd_t qt_meta_stringdata_HModifyProd = {
     {
 QT_MOC_LITERAL(0, 0, 11), // "HModifyProd"
-QT_MOC_LITERAL(1, 12, 15), // "getComponetsLot"
-QT_MOC_LITERAL(2, 28, 0), // ""
-QT_MOC_LITERAL(3, 29, 8), // "getIDLot"
-QT_MOC_LITERAL(4, 38, 13), // "getLotRowData"
-QT_MOC_LITERAL(5, 52, 9), // "updateRow"
-QT_MOC_LITERAL(6, 62, 9), // "deleteRow"
-QT_MOC_LITERAL(7, 72, 6), // "addRow"
-QT_MOC_LITERAL(8, 79, 23), // "on_pushButton_2_clicked"
-QT_MOC_LITERAL(9, 103, 21), // "on_pushButton_clicked"
-QT_MOC_LITERAL(10, 125, 23), // "on_pushButton_3_clicked"
-QT_MOC_LITERAL(11, 149, 22), // "on_radioButton_clicked"
-QT_MOC_LITERAL(12, 172, 24), // "on_radioButton_2_clicked"
-QT_MOC_LITERAL(13, 197, 23), // "on_pushButton_4_clicked"
-QT_MOC_LITERAL(14, 221, 27), // "on_cbUm_currentIndexChanged"
-QT_MOC_LITERAL(15, 249, 5), // "index"
-QT_MOC_LITERAL(16, 255, 23), // "on_pushButton_5_clicked"
-QT_MOC_LITERAL(17, 279, 20), // "on_deDal_dateChanged"
-QT_MOC_LITERAL(18, 300, 4), // "date"
-QT_MOC_LITERAL(19, 305, 19), // "on_deAl_dateChanged"
-QT_MOC_LITERAL(20, 325, 23), // "on_pushButton_6_clicked"
-QT_MOC_LITERAL(21, 349, 23), // "on_leSearch_textChanged"
-QT_MOC_LITERAL(22, 373, 4), // "arg1"
-QT_MOC_LITERAL(23, 378, 23), // "on_pushButton_7_clicked"
-QT_MOC_LITERAL(24, 402, 6), // "findIt"
-QT_MOC_LITERAL(25, 409, 3), // "arg"
-QT_MOC_LITERAL(26, 413, 17), // "on_rbprod_toggled"
-QT_MOC_LITERAL(27, 431, 7) // "checked"
+QT_MOC_LITERAL(1, 12, 16), // "getComponentsLot"
+QT_MOC_LITERAL(2, 29, 0), // ""
+QT_MOC_LITERAL(3, 30, 13), // "getLotRowData"
+QT_MOC_LITERAL(4, 44, 9), // "updateRow"
+QT_MOC_LITERAL(5, 54, 9), // "deleteRow"
+QT_MOC_LITERAL(6, 64, 6), // "addRow"
+QT_MOC_LITERAL(7, 71, 16), // "updateProduction"
+QT_MOC_LITERAL(8, 88, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(9, 112, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(10, 134, 23), // "on_pushButton_3_clicked"
+QT_MOC_LITERAL(11, 158, 22), // "on_radioButton_clicked"
+QT_MOC_LITERAL(12, 181, 24), // "on_radioButton_2_clicked"
+QT_MOC_LITERAL(13, 206, 23), // "on_pushButton_4_clicked"
+QT_MOC_LITERAL(14, 230, 27), // "on_cbUm_currentIndexChanged"
+QT_MOC_LITERAL(15, 258, 5), // "index"
+QT_MOC_LITERAL(16, 264, 23), // "on_pushButton_5_clicked"
+QT_MOC_LITERAL(17, 288, 20), // "on_deDal_dateChanged"
+QT_MOC_LITERAL(18, 309, 4), // "date"
+QT_MOC_LITERAL(19, 314, 19), // "on_deAl_dateChanged"
+QT_MOC_LITERAL(20, 334, 23), // "on_pushButton_6_clicked"
+QT_MOC_LITERAL(21, 358, 23), // "on_leSearch_textChanged"
+QT_MOC_LITERAL(22, 382, 4), // "arg1"
+QT_MOC_LITERAL(23, 387, 23), // "on_pushButton_7_clicked"
+QT_MOC_LITERAL(24, 411, 6), // "findIt"
+QT_MOC_LITERAL(25, 418, 3), // "arg"
+QT_MOC_LITERAL(26, 422, 17), // "on_rbprod_toggled"
+QT_MOC_LITERAL(27, 440, 7), // "checked"
+QT_MOC_LITERAL(28, 448, 25), // "on_pbUpdateAmount_clicked"
+QT_MOC_LITERAL(29, 474, 24), // "on_radioButton_2_toggled"
+QT_MOC_LITERAL(30, 499, 11), // "refreshData"
+QT_MOC_LITERAL(31, 511, 27) // "on_leQuantita_returnPressed"
 
     },
-    "HModifyProd\0getComponetsLot\0\0getIDLot\0"
+    "HModifyProd\0getComponentsLot\0\0"
     "getLotRowData\0updateRow\0deleteRow\0"
-    "addRow\0on_pushButton_2_clicked\0"
+    "addRow\0updateProduction\0on_pushButton_2_clicked\0"
     "on_pushButton_clicked\0on_pushButton_3_clicked\0"
     "on_radioButton_clicked\0on_radioButton_2_clicked\0"
     "on_pushButton_4_clicked\0"
@@ -73,7 +77,10 @@ QT_MOC_LITERAL(27, 431, 7) // "checked"
     "date\0on_deAl_dateChanged\0"
     "on_pushButton_6_clicked\0on_leSearch_textChanged\0"
     "arg1\0on_pushButton_7_clicked\0findIt\0"
-    "arg\0on_rbprod_toggled\0checked"
+    "arg\0on_rbprod_toggled\0checked\0"
+    "on_pbUpdateAmount_clicked\0"
+    "on_radioButton_2_toggled\0refreshData\0"
+    "on_leQuantita_returnPressed"
 };
 #undef QT_MOC_LITERAL
 
@@ -83,7 +90,7 @@ static const uint qt_meta_data_HModifyProd[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-      21,   14, // methods
+      25,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -91,35 +98,39 @@ static const uint qt_meta_data_HModifyProd[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,  119,    2, 0x08 /* Private */,
-       3,    0,  120,    2, 0x08 /* Private */,
-       4,    0,  121,    2, 0x08 /* Private */,
-       5,    0,  122,    2, 0x08 /* Private */,
-       6,    0,  123,    2, 0x08 /* Private */,
-       7,    0,  124,    2, 0x08 /* Private */,
-       8,    0,  125,    2, 0x08 /* Private */,
-       9,    0,  126,    2, 0x08 /* Private */,
-      10,    0,  127,    2, 0x08 /* Private */,
-      11,    0,  128,    2, 0x08 /* Private */,
-      12,    0,  129,    2, 0x08 /* Private */,
-      13,    0,  130,    2, 0x08 /* Private */,
-      14,    1,  131,    2, 0x08 /* Private */,
-      16,    0,  134,    2, 0x08 /* Private */,
-      17,    1,  135,    2, 0x08 /* Private */,
-      19,    1,  138,    2, 0x08 /* Private */,
-      20,    0,  141,    2, 0x08 /* Private */,
-      21,    1,  142,    2, 0x08 /* Private */,
-      23,    0,  145,    2, 0x08 /* Private */,
-      24,    1,  146,    2, 0x08 /* Private */,
-      26,    1,  149,    2, 0x08 /* Private */,
+       1,    0,  139,    2, 0x08 /* Private */,
+       3,    0,  140,    2, 0x08 /* Private */,
+       4,    0,  141,    2, 0x08 /* Private */,
+       5,    0,  142,    2, 0x08 /* Private */,
+       6,    0,  143,    2, 0x08 /* Private */,
+       7,    0,  144,    2, 0x08 /* Private */,
+       8,    0,  145,    2, 0x08 /* Private */,
+       9,    0,  146,    2, 0x08 /* Private */,
+      10,    0,  147,    2, 0x08 /* Private */,
+      11,    0,  148,    2, 0x08 /* Private */,
+      12,    0,  149,    2, 0x08 /* Private */,
+      13,    0,  150,    2, 0x08 /* Private */,
+      14,    1,  151,    2, 0x08 /* Private */,
+      16,    0,  154,    2, 0x08 /* Private */,
+      17,    1,  155,    2, 0x08 /* Private */,
+      19,    1,  158,    2, 0x08 /* Private */,
+      20,    0,  161,    2, 0x08 /* Private */,
+      21,    1,  162,    2, 0x08 /* Private */,
+      23,    0,  165,    2, 0x08 /* Private */,
+      24,    1,  166,    2, 0x08 /* Private */,
+      26,    1,  169,    2, 0x08 /* Private */,
+      28,    0,  172,    2, 0x08 /* Private */,
+      29,    1,  173,    2, 0x08 /* Private */,
+      30,    0,  176,    2, 0x08 /* Private */,
+      31,    0,  177,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Bool,
+    QMetaType::Bool,
+    QMetaType::Bool,
     QMetaType::Void,
-    QMetaType::Bool,
-    QMetaType::Bool,
-    QMetaType::Bool,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -135,6 +146,10 @@ static const uint qt_meta_data_HModifyProd[] = {
     QMetaType::Void,
     QMetaType::Void, QMetaType::QString,   25,
     QMetaType::Void, QMetaType::Bool,   27,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,   27,
+    QMetaType::Void,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -145,15 +160,15 @@ void HModifyProd::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id,
         auto *_t = static_cast<HModifyProd *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->getComponetsLot(); break;
-        case 1: _t->getIDLot(); break;
-        case 2: _t->getLotRowData(); break;
-        case 3: { bool _r = _t->updateRow();
+        case 0: _t->getComponentsLot(); break;
+        case 1: _t->getLotRowData(); break;
+        case 2: { bool _r = _t->updateRow();
             if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
-        case 4: { bool _r = _t->deleteRow();
+        case 3: { bool _r = _t->deleteRow();
             if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
-        case 5: { bool _r = _t->addRow();
+        case 4: { bool _r = _t->addRow();
             if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 5: _t->updateProduction(); break;
         case 6: _t->on_pushButton_2_clicked(); break;
         case 7: _t->on_pushButton_clicked(); break;
         case 8: _t->on_pushButton_3_clicked(); break;
@@ -169,6 +184,10 @@ void HModifyProd::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id,
         case 18: _t->on_pushButton_7_clicked(); break;
         case 19: _t->findIt((*reinterpret_cast< QString(*)>(_a[1]))); break;
         case 20: _t->on_rbprod_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 21: _t->on_pbUpdateAmount_clicked(); break;
+        case 22: _t->on_radioButton_2_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 23: _t->refreshData(); break;
+        case 24: _t->on_leQuantita_returnPressed(); break;
         default: ;
         }
     }
@@ -203,13 +222,13 @@ int HModifyProd::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 21)
+        if (_id < 25)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 21;
+        _id -= 25;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 21)
+        if (_id < 25)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 21;
+        _id -= 25;
     }
     return _id;
 }
